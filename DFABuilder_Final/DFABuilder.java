34c2ebf48a1890c0e1a9855f796a65df5a90dba6
-;C{I}O{O{R'X'{T}P}}"Hashbang! Games{INC}'Dæmon'Fœ'{œ}ter{R}'æmera::IPPH&&(&)I::- <shauntmcgovern@gmail.com>
2026-09-25 16:41:21 -0500

    CodeSpace work/'
    Java Github'{CodeSpaces}::-/';::-/';

--- DFABuilder_Final/DFABuilder.java
+++ DFABuilder_Final/DFABuilder.java
@@ -5,6 +5,22 @@
 //needed which is necessary for all. In order to remove, once again, any and all, violencia from the human population which is located in this 
 //github repo: https://github.com/shauntmcgovern/RemoveViolencePrototypesMSDTOpenSource
 
+//ls -la
+//cd DFABUilder_Final
+
+//pkill Xvfb
+//Xvfb :1 -screen 0 1024x768x16 &
+//x11vnc -display :1 -nopw -forever -listen localhost -xkb &
+//java -Dsun.java2d.pmoffscreen=false -Dsun.java2d.xrender=false DFABuilder
+//sudo apt-get update
+//sudo apt-get install -y novnc websockify
+//x11vnc -display :1 -nopw -forever -listen localhost -xkb &
+//websockify --web=/usr/share/novnc/ 6080 localhost:5900 &
+//java -Dsun.java2d.pmoffscreen=false -Dsun.java2d.xrender=false DFABuilder
+//then you can view the Java Intellisense GUI 'VNC' rendered at 'vnc_lite.html'(port 6080)
+//THe 'Project' was meant to understand MouseListeners and transverse thru' events with a interactive JavaRuntimeGUI Display.
+//The Deterministic Finite Automaton (DFA) NEIUSeniorProject
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
